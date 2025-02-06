1f5d295e64d3bd95ef93e47f428d7af9411db10d
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2025-02-06 18:56:35 +0530

    Create ParulUniversity.java

---
+++ EncapsulatinQspiders/ParulUniversity.java
@@ -0,0 +1,24 @@
+package HomeWork;
+
+public class ParulUniversity {
+	public static void main(String [] args) {
+		
+		Student s1 = new Student("Ching ",  80);
+		Student s2 = new Student("Lalit ",90);
+		Student s3 = new Student("kartik ",100);
+		Student s4 = new Student("Abishek ",100);
+		Student s5 = new Student("Harris ",100);
+		
+		s1.details();
+		s2.details();
+		s3.details();
+		s4.details();
+		s5.details();
+		
+	
+
+		
+		
+	}
+
+}
