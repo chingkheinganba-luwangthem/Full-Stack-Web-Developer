f74ea9f3d92f161a968fec62d3e962180edb615d
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2025-02-17 23:51:43 +0530

    Create ATM1.java

---
+++ Interface/ATM1.java
@@ -0,0 +1,9 @@
+package Interface;
+
+public interface ATM1 {
+	public abstract void deposit(int amt1);
+	                void withdraw(int amt2);
+	                void checkBalance();
+	                
+
+}
