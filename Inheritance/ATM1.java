d3691d0fd6b44030025eca905e57b95322bd73a2
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2025-02-17 23:52:33 +0530

    Delete Inheritance/ATM1.java

--- Inheritance/ATM1.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-public interface ATM1 {
-	public abstract void deposit(int amt1);
-	                void withdraw(int amt2);
-	                void checkBalance();
-	                
-
-}
